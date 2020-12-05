56bbd6d52658cf3248fc2c7a9fa3c95e63d8ff95
Iftee1711402 <tawsifhaque97@gmail.com>
2020-12-06 00:00:42 +0600

    python calculator program

---
+++ calc.py
@@ -0,0 +1,71 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+# function = operations["*"]
+# function(2, 3) # in this case, the function acts as the multiply function 
+
+def calculator():
+    print(f"\n{logo}")
+
+    num1 = float(input("\nenter num1: "))
+    for symbol in operations:
+        print(symbol)
+
+
+    to_continue = True
+    while to_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("enter next number: "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        user_choice = input(f"\ntype 'YES' to continue with {answer} \nor, type 'NEW' to start new calculation \nor, type 'NO' to exit: \n").lower()
+        if user_choice == 'no':
+            to_continue = False
+            print("goodbye!")
+            break
+
+        elif user_choice == 'yes':
+            num1 = answer
+            # then the loop goes back to the start of the while loop
+
+        elif user_choice == 'new':
+            calculator()
+            break
+
+calculator()
